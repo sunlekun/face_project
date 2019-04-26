fc42cd48ad143ef568572642a05832299dc10caa
yangwei007 <1610044378@qq.com>
2019-04-26 18:25:47 +0800

    提交

--- FaceDemo/src/main/java/com/demo/controller/LoginController.java
+++ FaceDemo/src/main/java/com/demo/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 /**
  * 
- * @author lekun.sun
+ * @author yangwei
  *
  */
 @Controller
