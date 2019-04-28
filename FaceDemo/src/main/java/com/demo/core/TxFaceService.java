9155c95f758ce482701ecb18b09a4864cecb59eb
sunlekun <53422330@qq.com>
2019-04-28 14:45:28 +0800

    SDK服务类

---
+++ FaceDemo/src/main/java/com/demo/core/TxFaceService.java
@@ -0,0 +1,14 @@
+package com.demo.core;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lekun.sun
+ * @version 创建时间：2019年4月28日 下午2:43:01
+ * @ClassName 类名称
+ * @Description 类描述：腾讯人脸识别调用服务
+ */
+@Component
+public class TxFaceService {
+
+}
