cf9792ac2aa19a67262d8c595a645716ec5eb830
sunlekun <53422330@qq.com>
2019-05-10 16:29:30 +0800

    Signed-off-by: sunlekun <53422330@qq.com>

--- FaceDemo/src/main/java/com/demo/util/DateFormatUtil.java
+++ FaceDemo/src/main/java/com/demo/util/DateFormatUtil.java
@@ -3,6 +3,7 @@ package com.demo.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -109,6 +110,16 @@ public class DateFormatUtil {
 					+ myDateTime.substring(4, 6);
 		}
 		return rtnDateTime;
+	}
+	
+	
+	/**
+	 *取当月第一天
+	 */
+	public static String dayOfMonth() throws Exception {
+		LocalDate today = LocalDate.now();//取当前时间
+		LocalDate firstDate = today.withDayOfMonth(1);//当月第一天
+		return firstDate.toString().replace("-", "");
 	}
 }
 
